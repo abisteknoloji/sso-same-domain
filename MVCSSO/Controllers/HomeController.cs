b62a2af7a314d4aa2a683bf6d3cdf8689d2ff892
Mahmut Gündoğdu <mahmutgundogdu@hotmail.com.tr>
2016-05-21 11:42:08 +0300

    first commit

---
+++ MVCSSO/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCSSO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Secured()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string name)
+        {
+
+            FormsAuthentication.SetAuthCookie(name,true);
+
+            return View();
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
---
+++ MVCSSO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSSO.MvcApplication" Language="C#" %>
